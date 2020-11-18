a8f575485c5699d161972a49512de27044808648
Daniel Garcia <danielgarciadan.dg@gmail.com>
2020-11-17 18:10:33 -0600

    login test

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT loginID, username, password FROM users WHERE username = ?";
+        $sql = "SELECT users.username, users.password , account.accountType FROM users JOIN account on users.loginID=account.loginID WHERE users.username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $account);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -61,10 +61,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["loginID"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
+                            $_SESSION["username"] = $username; 
+                            $_SESSION["accountType"]=$account;    
+                            // Redirect user to welcome page                       
+                            If($_SESSION["accountType"]==1){
+                                header("location: /adminFiles/doctorDashboard.php");
+                                exit();
+                            }
+                            If($_SESSION["accountType"]==1){
+                                header("location: /adminFiles/doctorDashboard.php");
+                                exit();
+                            }
+                            switch ($account) {
+                                case 1:
+                                    header("location: /adminFiles/doctorDashboard.php");
+                                    exit();
+                                    break;
+                                case 2:
+                                    header("location: welcome.php");
+                                    exit();
+                                    break;
+                                case 3:
+                                    header("location: /patientFiles/patientDashboard.php");
+                                    exit();
+                                    break;
+                            }
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";

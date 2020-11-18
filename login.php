9f233063a38490558d1ebe4516f58fe04503e78d
Daniel Garcia <danielgarciadan.dg@gmail.com>
2020-11-17 19:14:21 -0600

    login test 3

--- login.php
+++ login.php
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt,$username, $hashed_password, $account);
+                    $stmt->bind_result($username, $hashed_password, $account);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session

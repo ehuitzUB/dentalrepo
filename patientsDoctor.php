42ff54d642d002c25d6ca1804685a5d1e9360f90
Daniel Garcia <danielgarciadan.dg@gmail.com>
2020-12-03 12:55:14 -0600

    two tables test

--- patientsDoctor.php
+++ patientsDoctor.php
@@ -50,8 +50,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $patientDOB = mysqli_real_escape_string($link, $_REQUEST['patientDOB']);
  
 // Attempt insert query execution
-    $sql = "INSERT INTO account (firstname, lastname, telephone, accountType, DOB) VALUES ('$patientFName', '$patientLName', '$patientPhone', 3, '$patientDOB')";
-    if(mysqli_query($link, $sql)){
+    $sql = "INSERT INTO account (firstname, lastname, telephone, accountType, DOB) VALUES ('$patientFName', '$patientLName', '$patientPhone', 3, '$patientDOB');SELECT @last := LAST_INSERT_ID();INSERT INTO patient (accountID) VALUES ('@last')";
+    if(mysqli_multi_query($link, $sql)){
         header("location:patientsDoctor.php");
     } else{
         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);

9817836b88b5bec196a6d96055fecfec05b1953b
fehrfaber <fehrfaber@gmail.com>
2020-12-03 18:20:12 -0600

    added footer in login.php

--- login.php
+++ login.php
@@ -200,6 +200,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
     </div>
 </div> 
+<footer class="footer" style="position: absolute; bottom: 0px; width: 100%;">
+        <div class="container">
+            <div class="row justify-content-center mt-5" >
+            <p>Copyright &copy 2020 Twinkly Smiles Dentistry </p>
+            </div>
+        </div> 
+</footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
     integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
     crossorigin="anonymous"></script>
